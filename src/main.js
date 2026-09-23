064fa02dd25e27bafce214be39049e6a32370ae9
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-23 08:29:50 -0500

    chore: config primevue and vue-i18n

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Material from '@primeuix/themes/material'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+import {SelectButton} from "primevue";
+import i18n from "@/i18n.js";
 
-createApp(App).mount('#app')
+const primeUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
+createApp(App)
+    .use(i18n)
+    .use(PrimeVue, {
+        ripple: true,
+        license: primeUiLicenseKey,
+        theme: {
+            preset: Material
+        }
+    })
+    .component('pv-select-button', SelectButton)
+    .mount('#app')
